3fe8a790e61ccbf7b8eb5a14eb48691ff3dcab0d
Koci <kociandreas@seznam.cz>
2021-01-18 16:18:32 +0100

    Initial testing

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,9 +1,29 @@
 #include "MainComponent.h"
 
 //==============================================================================
-MainComponent::MainComponent()
+MainComponent::MainComponent() : pngComponent("E:\\GITHUB repositories\\ManageGame\\ManageGame\\defect4.png")
 {
-    setSize (600, 400);
+	this->setSize(600, 400);
+	this->addAndMakeVisible(pngComponent);
+
+	this->pngComponent.changeImage("E:\\GITHUB repositories\\ManageGame\\ManageGame\\ukol.png");
+
+	juce::Thread::launch([&]()
+		{
+			bool b = 0;
+			while (true)
+			{
+				juce::Thread::getCurrentThread()->wait(2000);
+				const juce::MessageManagerLock mmLock;
+
+				if (b == 0)
+					this->pngComponent.changeImage("E:\\GITHUB repositories\\ManageGame\\ManageGame\\ukol.png");
+				else
+					this->pngComponent.changeImage("E:\\GITHUB repositories\\ManageGame\\ManageGame\\defect4.png");
+				b = !b;
+			}
+		}
+	);
 }
 
 MainComponent::~MainComponent()
@@ -11,19 +31,11 @@ MainComponent::~MainComponent()
 }
 
 //==============================================================================
-void MainComponent::paint (juce::Graphics& g)
+void MainComponent::paint(juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+	this->pngComponent.setBounds(getLocalBounds());
 }
